977b3236e1f82c0785522ab08ebef22a42126ca9
Malte Meister <malte.meister@gmx.net>
2022-04-27 11:26:42 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="catma-py",
+    version="1.2.0",
+    author="Marco Petris",
+    packages=setuptools.find_packages(),
+    description="A suite of classes and functions to make Python scripts interoperable with CATMA",
+    url="https://github.com/forTEXT/catma-py",
+    python_requires=">=3.8",
+    install_requires=[]
+)
